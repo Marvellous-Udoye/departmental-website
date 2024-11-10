e2ed530e4caf3f898d9bb27f8c536c4e24811410
Marv_lous <155150860+Marvellous-Udoye@users.noreply.github.com>
2024-11-10 11:24:10 +0100

    feat: (sidebar.tsx and navbar.tsx) implement routing functionality to dashboard menu

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import Sidebar from "@/components/sidebar";
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-screen">
-      <Sidebar href={''} />
+      <Sidebar />
       <main className="absolute lg:left-[225px] max-w-[1124px] w-full">
         <Navbar />
         <div>
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -99,7 +99,7 @@ export default function SignIn() {
                   value={state.email}
                   onChange={handleChange}
                   placeholder="Enter email address"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-[12px]"
                 />
               </div>
             </div>
