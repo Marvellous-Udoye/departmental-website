6c40396d5c729bf22f76fa5143be7230b4885f2b
Ayoola Ibukunoluwa <ayoolaibukunoluwa@gmail.com>
2024-11-13 23:24:20 +0100

    feat(notifications page.tsx and drawer.tsx): implement notifications page UI and functionality

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -4,7 +4,13 @@ import { useState } from 'react'
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle, TransitionChild } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 
-export default function Drawer() {
+interface DrawerProps {
+  onClose?: () => void,
+  notificationDate: string,
+  notificationSubject: string,
+  notificationBody: string,
+}
+export default function Drawer({ notificationDate, notificationSubject, notificationBody, onClose }: DrawerProps) {
   const [open, setOpen] = useState(true)
 
   return (
@@ -38,7 +44,13 @@ export default function Drawer() {
                 <div className="px-4 sm:px-6">
                   <DialogTitle className="text-base font-semibold text-gray-900">Panel title</DialogTitle>
                 </div>
-                <div className="relative mt-6 flex-1 px-4 sm:px-6">{/* Your content */}</div>
+                <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                  <div className="p-4">
+                    <h2 className="text-lg font-bold">{notificationSubject}</h2>
+                    <p className="text-gray-500">{notificationDate}</p>
+                    <p className="mt-2">{notificationBody}</p>
+                  </div>
+                </div>
               </div>
             </DialogPanel>
           </div>
